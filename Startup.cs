6d5ba8ad3290e5574ed473033809cdf133286f2a
Marta <maxpetro4@gmail.com>
2020-08-14 14:31:59 +0200

    Appunto aspnet core mvc

--- Startup.cs
+++ Startup.cs
@@ -15,6 +15,7 @@ namespace MyCourse1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,13 +26,14 @@ namespace MyCourse1
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseStaticFiles();
-
-            app.Run(async (context) =>
+            //app.UseMVCWithDefaultRoute();
+            app.UseMvc(routeBuilder =>
             {
-                string nome = context.Request.Query["nome"];
-                await context.Response.WriteAsync($"Ciao {nome}!");
+                routeBuilder.MapRoute("default", "{controller=Home} / {action=index} / {id?}");
             });
+
+            app.UseStaticFiles();
+    
         }
     }
 }
